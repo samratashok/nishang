abb81fcd262197db5e0e075114b4f2d7da365ae9
samratashok <nikhil.uitrgpv@gmail.com>
2014-06-17 13:52:50 +0530

    Added encoded command generation to Invoke-Encode

--- Utility/Invoke-Encode.ps1
+++ Utility/Invoke-Encode.ps1
@@ -4,17 +4,26 @@ Script for Nishang to encode and compress plain data.
 
 .DESCRIPTION
 The script asks for a path to a plain file, encodes it and writes to a file "encoded.txt" in the current working directory.
-Both the encoding and decoding is based on the code by ikarstein.
+If the switch OutCommand is used. A command which could be used with "powershell.exe -encodedcommand" is also generated.
 
 .PARAMETER DataToEncode
 The path of the file to be decoded. Use with -IsString to enter a string.
 
-
 .PARAMETER OutputFilePath
 The path of the output file. Default is "encoded.txt" in the current working directory.
 
+.PARAMETER OutputCommandFilePath
+The path of the output file where encoded command would be written. Default is "encodedcommand.txt" in the current working directory.
+
 .PARAMETER IsString
 Use this to specify if you are passing a string ins place of a filepath.
+
+.EXAMPLE
+
+PS > Invoke-Encode -DataToEncode C:\files\encoded.txt -OutCommand
+
+Use above command to generate encoded data and encoded command which could be used with "powershell.exe -encodedcommand"
+
 
 .EXAMPLE
 
@@ -28,6 +37,7 @@ Use above to decode a string.
 
 .LINK
 http://blog.karstein-consulting.com/2010/10/19/how-to-embedd-compressed-scripts-in-other-powershell-scripts/
+http://www.darkoperator.com/blog/2013/3/21/powershell-basics-execution-policy-and-code-signing-part-2.html
 https://github.com/samratashok/nishang
 
 #>
@@ -44,6 +54,13 @@ function Invoke-Encode
         [Parameter(Position = 1, Mandatory = $False)]
         [String]
         $OutputFilePath = ".\encoded.txt", 
+
+        [Parameter(Position = 2, Mandatory = $False)]
+        [String]
+        $OutputCommandFilePath = ".\encodedcommand.txt",
+
+        [Switch]
+        $OutCommand,
 
         [Switch]
         $IsString
@@ -66,7 +83,26 @@ function Invoke-Encode
     $sw.Write($data)
     $sw.Close();
     $Compressed = [Convert]::ToBase64String($ms.ToArray())
-    $Compressed
-    Out-File -InputObject $Compressed -FilePath .\encoded.txt
-    Write-Host "Decode data written to $OutputFilePath"
+    Write-Verbose $Compressed
+    Out-File -InputObject $Compressed -FilePath $OutputFilePath
+    Write-Output "Encoded data written to $OutputFilePath"
+
+    if ($OutCommand -eq $True)
+    {
+        #http://www.darkoperator.com/blog/2013/3/21/powershell-basics-execution-policy-and-code-signing-part-2.html
+        $command = "Invoke-Expression `$(New-Object IO.StreamReader (" +
+
+        "`$(New-Object IO.Compression.GZipStream (" +
+
+        "`$(New-Object IO.MemoryStream (,"+
+
+        "`$([Convert]::FromBase64String('$Compressed')))), " +
+
+        "[IO.Compression.CompressionMode]::Decompress)),"+
+
+        " [Text.Encoding]::ASCII)).ReadToEnd();"
+        Write-Verbose $command
+        Out-File -InputObject $command -FilePath $OutputCommandFilePath
+        Write-Output "Encoded command written to $OutputCommandFilePath"
+    }
 }
