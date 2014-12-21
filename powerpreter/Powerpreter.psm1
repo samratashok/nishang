42912b23185a69221fc58e87d673d419c49df0fc
samratashok <nikhil.uitrgpv@gmail.com>
2014-12-21 10:40:27 +0530

    Updated Invoke-Encode/Decode in Powerpreter

--- powerpreter/Powerpreter.psm1
+++ powerpreter/Powerpreter.psm1
@@ -1383,25 +1383,24 @@ function ExetoText
 {
 <#
 .SYNOPSIS
-Payload to convert an executable to text file.
+Nishang script to convert an executable to text file.
 
 .DESCRIPTION
-This payload converts and an executable to a text file.
+This script converts and an executable to a text file.
 
 .PARAMETER EXE
-The executable to be converted.
+The path of the executable to be converted.
 
 .PARAMETER FileName
-Name of the text file to which executable will be converted.
+Path of the text file to which executable will be converted.
 
 .EXAMPLE
-PS > ExetoText evil.exe evil.txt
+PS > ExetoText C:\binaries\evil.exe C:\test\evil.txt
 
 .LINK
 http://www.exploit-monday.com/2011/09/dropping-executables-with-powershell.html
 https://github.com/samratashok/nishang
 #>
-
     [CmdletBinding()] Param(
         [Parameter(Position = 0, Mandatory = $True)]
         [String]
@@ -1411,9 +1410,9 @@ https://github.com/samratashok/nishang
         [String]
         $Filename
     )
-
     [byte[]] $hexdump = get-content -encoding byte -path "$EXE"
-    [System.IO.File]::WriteAllLines("$Filename", ([string]$hexdump))
+    [System.IO.File]::WriteAllLines($Filename, ([string]$hexdump))
+    Write-Output "Converted file written to $Filename"
 }
 
 
@@ -3708,41 +3707,70 @@ https://github.com/samratashok/nishang
 }
 
 ################################################Compress and Encode scripts and strings###############################
-
 function Invoke-Encode
 {
 <#
 .SYNOPSIS
-Functiob to encode and compress plain data.
+Script for Nishang to encode and compress plain data.
 
 .DESCRIPTION
-The function asks for a path to a plain file, encodes it and writes to a file "encoded.txt" in the current working directory.
+The script asks for a path to a plain file, encodes it and writes to a file "encoded.txt" in the current working directory.
+
+If the switch -OutCommand is used. An encoded command which could be executed on a non-powershell console is also generated.
+The encoded command is useful in case of non-interactive shells like webshell or when special characters in scripts may
+create problems, for example, a meterpreter session.
 
 .PARAMETER DataToEncode
 The path of the file to be decoded. Use with -IsString to enter a string.
 
-
 .PARAMETER OutputFilePath
 The path of the output file. Default is "encoded.txt" in the current working directory.
+
+.PARAMETER OutputCommandFilePath
+The path of the output file where encoded command would be written. Default is "encodedcommand.txt" in the current working directory.
 
 .PARAMETER IsString
 Use this to specify if you are passing a string ins place of a filepath.
 
+.PARAMETER OutCommand
+Generate an encoded command which could be used with -EncodedCommand parameter of PowerShell.
+
+.PARAMETER PostScriptCommand
+Generate a PowerShell command which is much smaller than encoded scripts. Useful in scenrios where
+longer commands or scripts could not be used. 
+
 .EXAMPLE
 
-PS > Invoke-Encode -DataToEncode C:\files\encoded.txt
+PS > Invoke-Encode -DataToEncode C:\scripts\data.txt
+
+Use above command to generate encoded data which could be Decoded using the Invoke-Decode script.
+
+
+PS > Invoke-Encode -DataToEncode C:\scripts\evil.ps1 -OutCommand
+
+Use above command to generate encoded data and encoded command which could be used on a non-powershell console.
+Use powershell -EncodedCommand <generated code here>
+
 
 .EXAMPLE
 
-PS > Invoke-Encode Get-Process -IsString
+PS > Invoke-Encode "A Secret message" -IsString
 
-Use above to decode a string.
+Use above to encode a string.
+
+
+.EXAMPLE
+
+PS > Invoke-Encode Get-Process -IsString -OutCommand
+
+Use above to encode a command.
+
 
 .LINK
-https://github.com/samratashok/nishang    
-#>    
-    
-    
+http://www.darkoperator.com/blog/2013/3/21/powershell-basics-execution-policy-and-code-signing-part-2.html
+https://github.com/samratashok/nishang
+
+#>
     [CmdletBinding()] Param(
         [Parameter(Position = 0, Mandatory = $True)]
         [String]
@@ -3752,8 +3780,19 @@ https://github.com/samratashok/nishang
         [String]
         $OutputFilePath = ".\encoded.txt", 
 
+        [Parameter(Position = 2, Mandatory = $False)]
+        [String]
+        $OutputCommandFilePath = ".\encodedcommand.txt",
+
         [Switch]
-        $IsString
+        $OutCommand,
+
+        [Switch]
+        $IsString,
+
+        [Switch]
+        $PostScriptCommand
+
     )
     if($IsString -eq $true)
     {
@@ -3763,8 +3802,10 @@ https://github.com/samratashok/nishang
     }
     else
     {
-        $Enc = Get-Content $DataToEncode -Encoding UTF8 
+        $Enc = Get-Content $DataToEncode -Encoding Ascii
     }
+
+
     #Compression logic from http://www.darkoperator.com/blog/2013/3/21/powershell-basics-execution-policy-and-code-signing-part-2.html
     $ms = New-Object IO.MemoryStream
     $action = [IO.Compression.CompressionMode]::Compress
@@ -3775,21 +3816,52 @@ https://github.com/samratashok/nishang
     
     # Base64 encode stream
     $Compressed = [Convert]::ToBase64String($ms.ToArray())
-    $Compressed
-    Out-File -InputObject $Compressed -FilePath .\encoded.txt
-    Write-Host "Decode data written to $OutputFilePath"
+    Out-File -InputObject $Compressed -FilePath $OutputFilePath
+    Write-Output "Encoded data written to $OutputFilePath"
+
+    if (($OutCommand -eq $True) -or ($PostScriptCommand -eq $True))
+    {
+        #http://www.darkoperator.com/blog/2013/3/21/powershell-basics-execution-policy-and-code-signing-part-2.html
+        $command = "Invoke-Expression `$(New-Object IO.StreamReader (" +
+
+        "`$(New-Object IO.Compression.DeflateStream (" +
+
+        "`$(New-Object IO.MemoryStream (,"+
+
+        "`$([Convert]::FromBase64String('$Compressed')))), " +
+
+        "[IO.Compression.CompressionMode]::Decompress)),"+
+
+        " [Text.Encoding]::ASCII)).ReadToEnd();"
+        
+        #Generate Base64 encoded command to use with the powershell -encodedcommand paramter"
+        $UnicodeEncoder = New-Object System.Text.UnicodeEncoding
+        $EncScript = [Convert]::ToBase64String($UnicodeEncoder.GetBytes($command))
+        #Check for max. length supported by Windows. If the base64 encoded command is longer use the other one.
+        if (($EncScript.Length -gt 8190) -or ($PostScriptCommand -eq $True))
+        {
+            Out-File -InputObject $command -FilePath $OutputCommandFilePath
+            Write-Output "Encoded command written to $OutputCommandFilePath"
+        }
+        else
+        {
+            Out-File -InputObject $EncScript -FilePath $OutputCommandFilePath
+            Write-Output "Encoded command written to $OutputCommandFilePath"
+        }
+    }
 }
 
 ################################################Decode scripts and strings encoded by Invoke-Encode###############################
+
 function Invoke-Decode
 {
 <#
 .SYNOPSIS
-Function to decode the data encoded by Invoke-Encode, DNS TXT and POST exfiltration methods.
+Script for Nishang to decode the data encoded by Invoke-Encode, DNS TXT and POST exfiltration methods.
 
 .DESCRIPTION
-The function asks for an encoded string as an option, decodes it and writes to a file "decoded.txt" in the current working directory.
-
+The script asks for an encoded string as an option, decodes it and writes to a file "decoded.txt" in the current working directory.
+Both the encoding and decoding is based on the code by ikarstein.
 
 .PARAMETER EncodedData
 The path of the file to be decoded. Use with -IsString to enter a string.
@@ -3807,11 +3879,12 @@ PS > Invoke-Decode -EncodedData C:\files\encoded.txt
 
 .EXAMPLE
 
-PS > Invoke-Decode K07MLUosSSzOyM+OycvMzsjM4eUCAA== -IsString
+PS > Invoke-Decode c08t0Q0oyk9OLS7m5QIA -IsString
 
 Use above to decode a string.
 
 .LINK
+http://www.darkoperator.com/blog/2013/3/21/powershell-basics-execution-policy-and-code-signing-part-2.html
 https://github.com/samratashok/nishang
 
 #>
@@ -3820,7 +3893,6 @@ https://github.com/samratashok/nishang
         [String]
         $EncodedData,
 
-       
         [Parameter(Position = 1, Mandatory = $False)]
         [String]
         $OutputFilePath = ".\decoded.txt", 
@@ -3846,7 +3918,6 @@ https://github.com/samratashok/nishang
     $cs = New-Object System.IO.Compression.DeflateStream ($ms, [System.IO.Compression.CompressionMode]::Decompress)
     $sr = New-Object System.IO.StreamReader($cs)
     $output = $sr.readtoend()
-    $output
     Out-File -InputObject $output -FilePath $OutputFilePath
     Write-Host "Decode data written to $OutputFilePath"
 }
